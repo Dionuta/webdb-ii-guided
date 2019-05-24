f71632978873602a886dfc22dc4236f9b3d226d5
Dionuta <diounta@gmail.com>
2019-05-23 20:16:21 -0700

    done with guided practice

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,28 +1,98 @@
+const knex = require('knex');
+
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  // get the roles from the database
-  res.send('Write code to retrieve all roles');
+const config = {
+  client: 'sqlite3',
+    connection: {
+        filename: './data/rolex.db3'
+    },
+  useNullAsDefault: true
+}
+
+const db = knex(config);
+
+function find() {
+  return db('roles')
+}
+
+function findById(id) {
+  return db('roles').where({id});
+}
+
+function add(user) {
+  return db('roles').insert(user)
+}
+
+function update(id, role) {
+  return db('roles')
+    .where('id', Number(id))
+    .update(role);
+}
+
+function remove(id) {
+  return db('roles')
+  .where('id', Number(id))
+  .del()
+}
+
+router.get('/', async (req, res) => {
+  try{
+    const roles = await find(req.params);
+    res.status(200).json(roles)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+router.get("/:id", async (req, res) => {
+  try {
+    const role = await findById(req.params.id);
+    if (role) {
+      res.status(200).json(role);
+    } else {
+      res
+        .status(404) 
+        .json({ message: "The role with the specified ID does not exist." });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: "The posts information could not be retrieved"
+    });
+  }
 });
 
-router.post('/', (req, res) => {
-  // add a role to the database
-  res.send('Write code to add a role');
+router.post('/', async (req, res) => {
+   try{
+     const newRole = await add(req.body)
+     res.status(201).json(newRole)
+   } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update roles
-  res.send('Write code to modify a role');
+router.put('/:id', async (req, res) => {
+   try{
+     const updatedRole = await update(req.params.id, req.body)
+     res.status(200).json(updatedRole)
+   } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // remove roles (inactivate the role)
-  res.send('Write code to remove a role');
+router.delete('/:id', async (req, res) => {
+  try {
+    const count =   await remove(req.params.id);
+    if(count > 0){
+        res.status(200).json({ message: "The role has been removed" });
+    }else{
+        res.status(404).json({ message: "The role could not be found" });
+    }
+}catch (err) {
+    res.status(500).json({ err });
+  }
 });
 
 module.exports = router;
